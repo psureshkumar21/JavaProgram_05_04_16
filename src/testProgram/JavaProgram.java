7361e832f1a011a3219e67547bc250e7c8b98679
suresh <IL94@global.tesco.org>
2016-04-06 18:22:27 +0530

    Latest Commit

--- src/testProgram/JavaProgram.java
+++ src/testProgram/JavaProgram.java
@@ -5,6 +5,7 @@ public class JavaProgram {
 	public static void main(String[] args) {
 		System.out.println("Hello Tesco");
 		System.out.println("Hello Team... Welcome to the New world");
+		System.out.println("Hare Rama Hare Rama Rama Rama Hare Hare");
 		
 	}
 
