6ea0458b3071dcb3f0ee98de7a09114dcc3acd3f
suresh <IL94@global.tesco.org>
2016-04-06 14:10:25 +0530

    New commit

--- src/testProgram/JavaProgram.java
+++ src/testProgram/JavaProgram.java
@@ -4,6 +4,8 @@ public class JavaProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Tesco");
+		System.out.println("Hello Team... Welcome to the New world");
+		
 	}
 
 }
